4577097603a2057de1791c8abfbd3a508893b371
Darito <davidalonso991@gmail.com>
2022-03-24 17:14:25 -0600

    Instalacion de axios y uso en app.jsx para obtener datos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import PhoneNumber from './components/PhoneNumber/PhoneNumber'
 
 function App() {
+  const [phoneNumbers, setPhoneNumbers] = useState([])
+  async function changeDataWithServerInfo() {
+    const promise = axios.get(import.meta.env.VITE_SERVER + '/api/phonenumbers')
+    promise.then(response => {
+      setPhoneNumbers(response.data)
+      return
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
 
+  useEffect(() => {
+    changeDataWithServerInfo()
+  }, [])
   return (
-    <div className="App">
-    </div>
+    <main className="App">
+      {phoneNumbers.map(phone => <PhoneNumber phone={phone} />)}
+    </main>
   )
 }
 
