cc8e4905d5e576182d821bdeeb345433ebac385b
Darito <davidalonso991@gmail.com>
2022-03-24 17:11:17 -0600

    Creacion de componente Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+function Header() {
+  return (
+    <header>
+      <h1>Agenda global</h1>
+      <p>¡Hola! <br />Esta es una agenda global donde puedes ingresar cualquier numero y guardarlo</p>
+    </header>
+  )
+}
+
+export default Header
